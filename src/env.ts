61622e18f3ee6365bbc11e253f929624e5308137
1F10220146 <s1f102201469@iniad.org>
2024-02-11 06:58:11 +0900

    chore: add env

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,12 @@
-import { createEnv } from '@t3-oss/env-core';
-import { z } from 'zod';
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
 
 export const env = createEnv({
-  runtimeEnv: process.env,
-  server: {
-    DISCORD_BOT_TOKEN: z.string(),
-    DISCORD_CLIENT_ID: z.string()
-  }
+	runtimeEnv: process.env,
+	server: {
+		DISCORD_BOT_TOKEN: z.string(),
+		DATABASE_URL: z.string(),
+		DISCORD_GUILD_ID: z.string(),
+		DISCORD_CLIENT_ID: z.string(),
+	},
 });
