67d8fbe9389f668a482ea5659b4f2bca734ec06f
1F10220146 <s1f102201469@iniad.org>
2024-02-18 01:46:36 +0900

    Refactor: showコマンドにActionBuilderを導入

--- src/commands/show.ts
+++ src/commands/show.ts
@@ -1,16 +1,12 @@
-import dayjs from "dayjs";
-import timezone from "dayjs/plugin/timezone"; // タイムゾーン用のプラグインをインポート
-import utc from "dayjs/plugin/utc";
 import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
 	ChatInputCommandInteraction,
-	EmbedBuilder,
 	SlashCommandBuilder,
 } from "discord.js";
 import { Command } from "../types";
-import officeAccessUseCase from "../usecase/officeAccessUsecase";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
+import { buildEmbed } from "../utils/generateShowMessage";
 
 const showCommand: Command<ChatInputCommandInteraction> = {
 	data: new SlashCommandBuilder()
@@ -18,38 +14,42 @@ const showCommand: Command<ChatInputCommandInteraction> = {
 		.setDescription("入室中のユーザーを表示します。") as SlashCommandBuilder,
 	execute: async (interaction) => {
 		await interaction.deferReply();
-		const unCheckedOutInfo = await officeAccessUseCase.show();
+		const embed = await buildEmbed();
 
-		const embed = new EmbedBuilder()
-			.setTitle("入室中のユーザー")
-			.setColor("#0099ff");
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+			new ButtonBuilder()
+				.setCustomId("refresh")
+				.setLabel("更新")
+				.setStyle(ButtonStyle.Primary),
+		);
 
-		const embedFields = unCheckedOutInfo.uncheckedOutRecords.map((record) => {
-			const user = unCheckedOutInfo.users.find(
-				(u) => u.user_id === record.user_id,
+		if (!interaction.channel) {
+			console.log("Channel is not available.");
+			await interaction.editReply(
+				"適切なチャンネルが見つかりませんでした。\n処理を中断します。",
 			);
-			if (user) {
-				const formattedCheckIn = dayjs(record.check_in)
-					.tz("Asia/Tokyo")
-					.format("YYYY/MM/DD HH:mm:ss");
+			return;
+		}
 
-				return {
-					name: user.user_name,
-					value: `入室時間: ${formattedCheckIn}`,
-				};
-			}
+		await interaction.editReply({ embeds: [embed], components: [row] });
+
+		const filter = (i) =>
+			i.customId === "refresh" && i.user.id === interaction.user.id;
+		const collector = interaction.channel.createMessageComponentCollector({
+			filter,
+			time: 15000,
 		});
 
-		for (const field of embedFields) {
-			if (field) {
-				embed.addFields(field);
+		collector.on("collect", async (i) => {
+			if (i.customId === "refresh") {
+				const updatedEmbed = await buildEmbed();
+				if (!interaction.channel) {
+					console.log("Channel is not available.");
+					return;
+				}
+				await i.update({ embeds: [updatedEmbed], components: [row] });
 			}
-		}
-		if (embedFields.length === 0) {
-			embed.setDescription("入室中のユーザーはいません。");
-		}
-
-		await interaction.editReply({ embeds: [embed] });
+		});
 	},
 };
 
---
+++ src/utils/generateShowMessage.ts
@@ -0,0 +1,41 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import { EmbedBuilder } from "discord.js";
+import officeAccessUseCase from "../usecase/officeAccessUsecase";
+
+dayjs.extend(timezone);
+
+export const buildEmbed = async () => {
+	const unCheckedOutInfo = await officeAccessUseCase.show();
+	const embed = new EmbedBuilder()
+		.setTitle("入室中のユーザー")
+		.setColor("#0099ff");
+
+	const embedFields = unCheckedOutInfo.uncheckedOutRecords
+		.map((record) => {
+			const user = unCheckedOutInfo.users.find(
+				(u) => u.user_id === record.user_id,
+			);
+			if (user) {
+				const formattedCheckIn = dayjs(record.check_in)
+					.tz("Asia/Tokyo")
+					.format("YYYY/MM/DD HH:mm:ss");
+				return { name: user.user_name, value: `入室時間: ${formattedCheckIn}` };
+			}
+			return null;
+		})
+		.filter((field) => field !== null);
+
+	if (embedFields.length === 0) {
+		embed.setDescription("入室中のユーザーはいません。");
+	}
+	for (const field of embedFields) {
+		if (field) {
+			embed.addFields(field);
+		}
+	}
+	const updatedTime = dayjs().tz("Asia/Tokyo").format("YYYY/MM/DD HH:mm:ss");
+	embed.setFooter({ text: `最終更新: ${updatedTime}` });
+
+	return embed;
+};
