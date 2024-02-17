6499f495fb12147aeb70cbc7bb2805130f6093b8
1F10220146 <s1f102201469@iniad.org>
2024-02-17 23:18:14 +0900

    fix: format

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, GatewayIntentBits } from "discord.js";
 
-import { env } from './env';
-import { getCommands, getEvents } from './utils/core';
+import { env } from "./env";
+import { getCommands, getEvents } from "./utils/core";
 
 export const client = new Client({
-  /**
-   * If your bot requires certain intents, you can specify them here.
-   * @see https://discordjs.guide/popular-topics/intents.html
-   */
-  intents: [
-    GatewayIntentBits.Guilds // For our example `/purge` command
-  ]
+	/**
+	 * If your bot requires certain intents, you can specify them here.
+	 * @see https://discordjs.guide/popular-topics/intents.html
+	 */
+	intents: [
+		GatewayIntentBits.Guilds, // For our example `/purge` command
+	],
 });
 
 client.commands = new Collection();
@@ -20,11 +20,11 @@ commands.forEach((command) => client.commands.set(command.data.name, command));
 
 const events = await getEvents();
 events.forEach((event) => {
-  if (event.runOnce) {
-    client.once(event.name, event.execute);
-  } else {
-    client.on(event.name, event.execute);
-  }
+	if (event.runOnce) {
+		client.once(event.name, event.execute);
+	} else {
+		client.on(event.name, event.execute);
+	}
 });
 
 client.login(env.DISCORD_BOT_TOKEN);
