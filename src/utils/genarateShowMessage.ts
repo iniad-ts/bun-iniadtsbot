36c35effcae377f867eba4e750500f95d890e997
1F10220146 <s1f102201469@iniad.org>
2024-04-24 14:30:01 +0900

    fix:タイムゾーンを適応

--- src/utils/genarateShowMessage.ts
+++ src/utils/genarateShowMessage.ts
@@ -1,16 +1,21 @@
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
 import officeAccessUseCase from "../usecase/officeAccessUseCase";
+
+dayjs.extend(timezone);
 
 const genarateShowMessage = async () => {
   const userList = await officeAccessUseCase.show();
   let messageContent = "現在入室中のユーザー:\n";
   // ユーザーリストを文字列に整形
   userList.forEach((user, index) => {
-    messageContent += `${index + 1}. ${user.userName} - ${dayjs(
-      user.checkIn,
-    ).format("YYYY/MM/DD HH:mm:ss")}\n`;
+    messageContent += `${index + 1}. ${user.userName} - ${dayjs(user.checkIn)
+      .tz("Asia/Tokyo")
+      .format("YYYY/MM/DD HH:mm:ss")}\n`;
   });
-  messageContent += `\n最終更新日時: ${dayjs().format("YYYY/MM/DD HH:mm:ss")}`;
+  messageContent += `\n最終更新日時: ${dayjs()
+    .tz("Asia/Tokyo")
+    .format("YYYY/MM/DD HH:mm:ss")}`;
 
   return messageContent;
 };
